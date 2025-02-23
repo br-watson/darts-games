8865a2720fe0a82ab6ec62b8d7163a994ce73a16
br-watson <129091822+br-watson@users.noreply.github.com>
2025-02-23 20:39:14 +0000

    add back the animated background

--- src/components/darts/DartsGames.tsx
+++ src/components/darts/DartsGames.tsx
@@ -2,13 +2,17 @@
 
 import { X01Game } from '@/components/darts/x01/X01Game';
 import { PlayerProfileProvider } from '@/components/darts/x01/context/PlayerProfileContext';
+import AnimatedBackground from '@/components/darts/common/components/AnimatedBackground';
 
 export const DartsGames: React.FC = () => {
     return (
-        <PlayerProfileProvider>
-            <div className="container mx-auto p-4">
-                <X01Game />
-            </div>
-        </PlayerProfileProvider>
+        <>
+            <AnimatedBackground/>
+            <PlayerProfileProvider>
+                <div className="container mx-auto p-4">
+                    <X01Game />
+                </div>
+            </PlayerProfileProvider>
+        </>
     );
 };
